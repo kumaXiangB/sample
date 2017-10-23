63648b0f23e3f1c10f3c21195cc31cee7ea6e0dd
kumaxiangb <524617860@qq.com>
2017-10-23 09:41:44 +0000

    Add account activations

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,5 @@ Route::resource('users', 'UsersController');
 Route::get('login', 'SessionsController@create')->name('login');
 Route::post('login', 'SessionsController@store')->name('login');
 Route::delete('logout', 'SessionsController@destroy')->name('logout');
-Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
+
+Route::get('signup/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_email');
